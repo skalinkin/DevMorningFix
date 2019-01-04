841e7476f48e1ee9a042666b0edfd01d3b630d34
skalinkin <skalinkin@gmail.com>
2019-01-04 08:17:55 -0500

    Add TODO item to execute.

---
+++ Source/Infrastructure/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace Avtec.DevMorningFix.Infrastructure
+{
+    public static class Bootstrapper
+    {
+        public static void Configure(IContainer container)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            for (var i = 0; i < appSettings.Count; i++)
+            {
+                var sInterfaceName = appSettings.GetKey(i);
+                var sImplName = appSettings[i];
+
+                var ifType = Type.GetType(sInterfaceName, true, true);
+                var implType = Type.GetType(sImplName, true, true);
+
+                container.Register(ifType, implType);
+            }
+        }
+    }
+}
