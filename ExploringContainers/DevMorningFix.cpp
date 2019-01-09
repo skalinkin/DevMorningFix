40034c5a4ff374b1c7970af5a952c197486b03f8
Sergiy Kalinkin <skalinkin@gmail.com>
2019-01-09 10:44:21 -0500

    Adressing TODO to separate application from execution environment

---
+++ ExploringContainers/DevMorningFix.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "DevMorningFix.h"
+#include <iostream>
+
+
+DevMorningFix::DevMorningFix()
+{
+}
+
+void DevMorningFix::Start()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+DevMorningFix::~DevMorningFix()
+{
+}
