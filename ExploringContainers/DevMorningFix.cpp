d0756bf06402e655b30ed4c2cce8202e96b9e4fd
Yackey <syackey-qa@qa.avtecinc.com>
2019-01-09 11:43:07 -0500

    use smart pointer; provide all constructors

--- ExploringContainers/DevMorningFix.cpp
+++ ExploringContainers/DevMorningFix.cpp
@@ -8,7 +8,7 @@ DevMorningFix::DevMorningFix(std::ostream& output)
 
 void DevMorningFix::Start()
 {
-	std::cout << "Hello World!\n"; 
+	std::cout << "Hello World! C++ is waaaay cool.\n"; 
 }
 
 DevMorningFix::~DevMorningFix()
--- ExploringContainers/DevMorningFix.h
+++ ExploringContainers/DevMorningFix.h
@@ -4,6 +4,18 @@
 class DevMorningFix
 {
 public:
+	// if you don't provide these constructors, the compiler will (silently)
+	// its good form to explicitly declare these
+	// if you have nothing brilliant to add, then delete or default is ok
+	//
+	// Let's put these in and see if it affects any containers.
+	// 
+	DevMorningFix() = delete;
+	DevMorningFix(DevMorningFix&&) = default;
+	DevMorningFix& operator=(DevMorningFix&&) = default;	// move
+	DevMorningFix(const DevMorningFix&) = default;
+	DevMorningFix&operator=(const DevMorningFix&) = default;	// copy
+
 	DevMorningFix(std::ostream& output);
 	virtual ~DevMorningFix();
 	static void Start();
