00a02b0b857d82791233aa7004a91e85c947db33
Yackey <syackey-qa@qa.avtecinc.com>
2019-01-19 06:50:33 -0500

    more flexible resolver factory

--- Source/ExploringContainers/DependencyResolverFactory.cpp
+++ Source/ExploringContainers/DependencyResolverFactory.cpp
@@ -3,23 +3,65 @@
 #include "IDependencyResolver.h"
 #include "HypodermicAdapter.h"
 #include "BoostDIAdapter.h"
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/optional.hpp>
+#include <boost/property_tree/ptree_fwd.hpp>
 
 using namespace std;
 
-std::unique_ptr<IDependencyResolver> DependencyResolverFactory::CreateDependencyResolver(const string & resolverTypeName)
+std::string DependencyResolverFactory::m_sFilePath;
+std::shared_ptr<boost::property_tree::ptree> DependencyResolverFactory::m_pInputXMLTree;
+
+std::unique_ptr<IDependencyResolver> DependencyResolverFactory::CreateDependencyResolver() const
 {
+	string sContainerName = GetContainerName();
 	// TODO: this need to be some form of map lookup
 	// 
 	unique_ptr<IDependencyResolver> upDr;
 	int nCmp = -1;
 
-	if ((nCmp = resolverTypeName.compare("hypodermic")) == 0)
+	if ((nCmp = sContainerName.compare("hypodermic")) == 0)
 	{
 		upDr.reset(new HypodermicAdapter());
 	}
-	else if ((nCmp = resolverTypeName.compare("boostdi")) == 0)
+	else if ((nCmp = sContainerName.compare("boostdi")) == 0)
 	{
 		upDr.reset(new BoostDIAdapter());
 	}
+	else
+	{
+		std::stringstream errMsg;
+		errMsg << "Container name '" << sContainerName << "' is not valid.";
+		throw std::exception(errMsg.str().c_str());
+	}
 	return upDr;
+}
+
+std::string DependencyResolverFactory::GetContainerName() const
+{
+	string sContainerName;
+	string sFilePath = ".\\props.xml";
+	boost::property_tree::ptree *pInputXMLTree = new boost::property_tree::ptree();
+	// avoid &#10 and &#9 garbage with flags
+	read_xml(sFilePath, *pInputXMLTree, (boost::property_tree::xml_parser::no_comments | boost::property_tree::xml_parser::trim_whitespace));
+	const boost::optional<std::string> boContainerName =
+		pInputXMLTree->get_optional<std::string>("configuration.appSettings.container.<xmlattr>.name");
+
+	if (boContainerName)
+	{
+		sContainerName = boContainerName.get();
+	}
+	else
+	{
+		std::stringstream errMsg;
+		errMsg << "Could not find container name in '" << sFilePath << "'";
+		throw std::exception(errMsg.str().c_str());
+	}
+	return sContainerName;
+}
+
+DependencyResolverFactory::DependencyResolverFactory()
+{
+	m_sFilePath = ".\\props.xml";
+	m_pInputXMLTree.reset(new boost::property_tree::ptree());
 }
--- Source/ExploringContainers/DependencyResolverFactory.h
+++ Source/ExploringContainers/DependencyResolverFactory.h
@@ -2,19 +2,20 @@
 #include <string>
 #include <map>
 #include "IDependencyResolver.h"
+#include <boost/property_tree/ptree_fwd.hpp>
 
 class DependencyResolverFactory
 {
 public:
-	static DependencyResolverFactory *GetInstance()
-	{
-		static DependencyResolverFactory instance;
-		return &instance;
-	}
-	static std::unique_ptr<IDependencyResolver>CreateDependencyResolver(const std::string &resolverTypeName);
-private:
-	DependencyResolverFactory() = default;
+	DependencyResolverFactory();
 	~DependencyResolverFactory() = default;
 	DependencyResolverFactory(const DependencyResolverFactory &) = default;
 	DependencyResolverFactory &operator=(const DependencyResolverFactory &) = default;
+
+	std::string GetContainerName() const;
+	std::unique_ptr<IDependencyResolver>CreateDependencyResolver() const;
+
+private:
+	static std::string m_sFilePath;
+	static std::shared_ptr<boost::property_tree::ptree> m_pInputXMLTree;
 };
