5a4676e376296d3d28eafa84ceaaf1802cf93eaa
peagerm@gmail.com <peagerm@gmail.com>
2019-01-28 15:27:34 -0500

    Structuremap config added.

---
+++ Source/Container/StructureMap/StructureMapDependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Composition;
+
+namespace Avtec.DevMorningFix.Container.StructureMap
+{
+    [Export(typeof(IDependencyResolver))]
+    [ExportMetadata("Name", "StructureMap")]
+    class StructureMapDependencyResolver : IDependencyResolver
+    {
+        public IStartup GetCompositionRoot1()
+        {
+            var container = new global::StructureMap.Container(
+                _ => _.Scan
+                    (x =>
+                {
+                    x.TheCallingAssembly();
+                    x.AssembliesFromApplicationBaseDirectory();
+                    x.WithDefaultConventions();
+                    x.LookForRegistries();
+                })
+                );
+
+
+            var startup = container.GetInstance<IStartup>();
+            return startup;
+        }
+        public IStartup GetCompositionRoot()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
