3f5eeb9f412941c55179970c5c02ab9d2cb39cfc
Rachel Byrd <rbyrd@avtecinc.com>
2019-01-29 09:20:17 -0500

    Adding asp.net web form application to the project

---
+++ Source/WebApplication/WebStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Avtec.DevMorningFix.BusinessCases;
+using Avtec.DevMorningFix.FormatOutput;
+
+namespace Avtec.DevMorningFix.WebApp
+{
+    public class WebStartup : IStartup
+    {
+        private readonly IShowFundamentals _showFundamentalsBusinessCase;
+        //private readonly IOutput outputOut;
+
+       
+        public WebStartup(IShowFundamentals showFundamentalsBusinessCase)
+        {
+            _showFundamentalsBusinessCase = showFundamentalsBusinessCase;
+        }
+
+        public void Start(params object[] parameters)
+        {
+            _showFundamentalsBusinessCase.Execute();
+
+        }
+    }
+}
