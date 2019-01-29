f6eeb2505207e6039dba2995b86e19253dd8d646
skalinkin <skalinkin@gmail.com>
2019-01-29 09:25:57 -0500

    Converting output constructor parameter to be set throug property

--- Source/ConsoleApp/ConsoleStartup.cs
+++ Source/ConsoleApp/ConsoleStartup.cs
@@ -5,15 +5,18 @@ namespace Avtec.DevMorningFix.ConsoleApp
 {
     internal class ConsoleStartup : IStartup
     {
+        private readonly IOutput _output;
         private readonly IShowFundamentals _showFundamentalsBusinessCase;
 
-        public ConsoleStartup(IShowFundamentals showFundamentalsBusinessCase)
+        public ConsoleStartup(IShowFundamentals showFundamentalsBusinessCase,IOutput output)
         {
+            _output = output;
             _showFundamentalsBusinessCase = showFundamentalsBusinessCase;
         }
 
         public void Start(params object[] parameters)
         {
+            _showFundamentalsBusinessCase.SetOutput(_output);
             _showFundamentalsBusinessCase.Execute();
             Console.ReadLine();
         }
