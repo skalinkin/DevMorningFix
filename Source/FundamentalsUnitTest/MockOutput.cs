dc021a03d54a583a15446b5c7006e224a16e8da3
Wayne Smith <wsmith@avtecinc.com>
2019-02-01 08:04:14 -0500

    added first unit tests

---
+++ Source/FundamentalsUnitTest/MockOutput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Avtec.DevMorningFix;
+
+namespace FundamentalsUnitTest
+{
+    class MockOutput : IOutput
+    {
+        public void OutputFormatted(IDevFixFundamental f)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OutputData(IEnumerable<IDevFixFundamental> fundamentals)
+        {
+            Fundamentals = fundamentals;
+        }
+
+        public IEnumerable<IDevFixFundamental> Fundamentals;
+    }
+}
---
+++ Source/FundamentalsUnitTest/ShowFundamentalsTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Avtec.DevMorningFix.BusinessCases.Impl;
+using FundamentalsUnitTest;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Avtec.DevMorningFix.FundamentalsUnitTest
+{
+    [TestFixture]
+    internal class ShowFundamentalsTests
+    {
+        [Test]
+        public void OutputNotSet()
+        {
+            // Arrange
+            var repository = Substitute.For<IFundamentalRepository>();
+            var subject = new ShowFundamentals(repository);
+            subject.SetOutput(null);
+
+            // Act
+            var exceptionThrown = false;
+            try
+            {
+                subject.Execute();
+            }
+            catch (InvalidOperationException ex)
+            {
+                exceptionThrown = true;
+            }
+
+            // Assess
+            Assert.IsTrue(exceptionThrown);
+        }
+
+        [Test]
+        public void ResultNotNull()
+        {
+            // Arrange
+            var repository = Substitute.For<IFundamentalRepository>();
+            var list = new List<DevFixFundamental>();
+            repository.GetAllFundamentals().Returns(list);
+
+            var subject = new ShowFundamentals(repository);
+
+            var output = new MockOutput();
+            output.Fundamentals = null;
+            subject.SetOutput(output);
+
+            // Act
+            subject.Execute();
+
+            // Assess
+            Assert.IsNotNull(output.Fundamentals);
+        }
+    }
+}
