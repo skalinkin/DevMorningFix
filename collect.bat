110610f6378d4ea726ef2e28ac6badc79885cf59
Nick <nmauro@avtecinc.com>
2019-02-04 16:14:51 -0500

    Fixed the touch command, because not everyone has that on thier Winderz installations :) Now using the Windoze alternative, copy command hack.

--- collect.bat
+++ collect.bat
@@ -1,5 +1,5 @@
 
 If Not exist .\Source\ExploringContainers\conanbuildinfo_release_x64_v141.props (
 	conan install -if .\Source\ExploringContainers .\Source\ExploringContainers
-	touch DevMorningFix.sln
+	copy /b DevMorningFix.sln +,, 
 )
