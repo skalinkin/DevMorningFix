2fbe0b3a61ad46abc07ad3b19ebebd37f7a48840
Nick <nmauro@avtecinc.com>
2019-02-04 11:34:20 -0500

    Added new commands file for executing commands when a project is loaded. The VS extenstion Command_Task_Runner_v1.2.41 is required. The command that's run will execute the conan install and touch the .sln file which will force the solution to be reloaded (asking the user first). This will load the new .props files in order to load the ExploringContainers project.

--- collect.bat
+++ collect.bat
@@ -1,1 +1,5 @@
-conan install -if .\Source\ExploringContainers .\Source\ExploringContainers
+
+If Not exist .\Source\ExploringContainers\conanbuildinfo_release_x64_v141.props (
+	conan install -if .\Source\ExploringContainers .\Source\ExploringContainers
+	touch DevMorningFix.sln
+)
